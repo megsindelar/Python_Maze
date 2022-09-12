718bfbe0ba1712fb88e0315ee8ab87a3fa9246aa
David <daviddorf2023@u.northwestern.edu>
2022-09-12 10:17:31 -0500

    added wavematrix and wallmatrix, initialized vars

--- maze.py
+++ maze.py
@@ -1,4 +1,22 @@
 import numpy as np
 
-a2D = np.array([[0, 0, 0, 1], [1, 0, 1, 1], [0, 0, 0, 0], [0, 1, 0, 0]])
-print(a2D)
+wallmat = np.array([[0, 0, 0, -1], [-1, 0, -1, -1], [0, 0, 0, 0], [0, -1, 0, 0]])
+wavemat = np.zeros((4,4))
+print(wavemat)
+
+#robotpos = wallmat[0, 0]
+#robotpos = 2
+
+#print(wavemat)
+
+robotpos = [0,0]
+robotval = 7
+
+wallmat[robotpos[0],robotpos[1]] = robotval
+print(wallmat)
+
+
+
+
+
+
