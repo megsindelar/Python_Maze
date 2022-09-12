e1d7cf347d2866e68243732cce70d76d25d5e9a1
Meg <megansindelar2023@u.northwestern.edu>
2022-09-12 16:58:51 -0500

    Minor bug fixes, but need to loop so that the robot reaches the goal

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from cgitb import small
 from operator import ne
+import re
 from shutil import move
 from tabnanny import check
 import wave
@@ -26,7 +27,31 @@ import numpy as np
 
 
 
+wallmat = np.array([[-1, -1, -1, -1, -1, -1], [-1, 0, 0, 0, -1, -1], [-1, -1, 0, -1, -1, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, -1, 0, 0, -1]])
+wavemat = np.zeros((6,6))
 
+wallmat = np.array([[-1, -1, -1, -1, -1, -1], [-1, 0, 2, 0, -1, -1], [-1, -1, 0, -1, -1, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, -1, 0, 3, -1], [-2, -2, -2, -2, -2, -2]])
+wavemat = np.array([[-2, -2, -2, -2, -2, -2], [-2, 6, 5, 6, -1, -2], [-2, -1, 4, -1, -1, -2], [-2, 4, 3, 2, 1, -2], [-2, 5, -1, 1, 0, -2], [-2, -2, -2, -2, -2, -2]])
+print(wavemat)
+
+
+
+#robotpos = wallmat[0, 0]
+#robotpos = 2
+
+
+
+#print(wavemat)
+
+#robotpos = [0,0]
+#robotval = 7
+
+#wallmat[robotpos[0],robotpos[1]] = robotval
+print(wallmat)
+
+
+
+#keep track of what index goal is at and what index the robot is at and if it's at goal then it's done
 
 
 ###################################################
@@ -35,82 +60,76 @@ import numpy as np
 
 """Find shortest path"""
 def shortest_path():
-    robot_move = wavemat
-    m = 0
-
-    """Place robot at start"""
-    #robotpos = [0,startc]
-    #i = 0
-    #j = startc
-
-    """Directions robot can move"""
-    north = robot_move[(i+1), j]
-    east = robot_move[i, (j+1)]
-    south = robot_move[(i-1), j]
-    west = robot_move[i, (j-1)]
-
-
-    """Finding the smallest numeric value for robot path"""
-    lst = [north, east, south, west]
-    lst_str = [str(x) for x in lst]
-    smallest_num = lst.sort()
-    smallest = [str(x) for x in smallest_num]
-
-
     """Determine where the robot should move"""
     def move_robot(ind):
+        global i,j, robotpos
+        print(robotpos)
+        print(i)
+        #print(j)
         if ind == 0:
             """Robot position movement north"""
-            robotpos = wavemat[(i+1),j]
+            robotpos = [(i+1),j]
             i+=1
         elif ind == 1:
             """Robot position movement east"""
-            robotpos = wavemat[i,(j+1)]
+            robotpos = [i,(j+1)]
             j+=1
         elif ind == 2:
             """Robot position movement south"""
-            robotpos = wavemat[(i-1), j]
+            robotpos = [(i-1), j]
             i-=1
         else:
             """Robot position movement west"""
-            robotpos = wavemat[i, (j-1)]
+            robotpos = [i, (j-1)]
             j-=1
 
     def reverse_robot(ind):
+        global i,j
         if ind == 0:
             """Reverse robot position movement from north"""
-            robotpos = wavemat[(i-1),j]
+            robotpos = [(i-1),j]
             i-=1
         elif ind == 1:
             """Reverse robot position movement from east"""
-            robotpos = wavemat[i,(j-1)]
+            robotpos = [i,(j-1)]
             j-=1
         elif ind == 2:
             """Reverse robot position movement from south"""
-            robotpos = wavemat[(i+1), j]
+            robotpos = [(i+1), j]
             i+=1
         else:
             """Reverse robot position movement from west"""
-            robotpos = wavemat[i, (j+1)]
+            robotpos = [i, (j+1)]
             j+=1
 
-    
-    def pos_vals_rob(l):
-        """Checking position values"""
-        if (lst_str.__contains__("3")):
-            """Check if robot is next to the goal (3 represents goal)"""
-            list_index = lst_str.index("3")
-            move_robot(list_index)
-            print("Reached goal!")
+    def check_goal():
+        global robotpos
+        close = np.subtract(robotpos,goal)
+        if (abs(close[0] < 2) and close[1] == 0) or (close[0] == 0 and abs(close[1] < 2)):
+            """If next to goal, move to goal"""
+            robotpos = goal
+            ifgoal = 1
+            return ifgoal
+        else:
+            ifgoal = 0
+            return ifgoal
 
-        elif (smallest_num[0] == -1):
+    def pos_vals_rob(l):
+        global length, last_val, next_val, next_ind, reached_goal
+        """Checking position values"""
+        ifgoal = check_goal()
+        if (ifgoal == 1):
+            print("Reached goal!")
+            reached_goal = 1
+
+        elif (smallest_num[0] == -1) or (smallest_num[0] == -2):
             """Check if robot is next to a wall (-1 represents a wall)"""
             check_wall = smallest_num[1]
-            i = 1
-            while check_wall == -1:
-                i+=1
-                check_wall = smallest_num[i]
-                if(i == 3) & (check_wall == -1):
+            c = 1
+            while check_wall == -1 or check_wall == -2:
+                c+=1
+                check_wall = smallest_num[c]
+                if(c == 3) & (check_wall == -1 or check_wall == -2):
                     print("Boxed in! No way out!")
                     break
             next_val = check_wall
@@ -118,24 +137,25 @@ def shortest_path():
             if (l == 0):
                 next_ind = []
                 index_pos = 0
-                for i in lst:
+                m = 0
+                for a in lst:
                     if check_wall in lst:
                         next_index = lst.index(check_wall, index_pos)
                         next_ind.append(next_index)
-                        len = len(next_ind)
-            
+                        length = len(next_ind)
+                    
             """Checking if multiple of the same smallest cell values"""
-            if (len>1) & (l == 0):
+            if (length>1) & (l == 0):
                 move_robot(next_ind[m])
                 m+=1
                 l+=1
-                len -= 1
+                length -= 1
                 last_val = check_wall
                 pos_vals_rob(l)
-            elif (len>1) & (l>0) & (next_val > last_val):
+            elif (length>1) & (l>0) & (next_val > last_val):
                 reverse_robot(next_ind[m-1])
                 move_robot(next_ind[m])
-                len-=1
+                length-=1
                 m+=1
                 l+=1
                 last_val = check_wall
@@ -154,36 +174,89 @@ def shortest_path():
             if (l == 0):
                 next_ind = []
                 index_pos = 0
+                m = 0
                 for i in lst:
                     if check_wall in lst:
                         next_index = lst.index(check_wall, index_pos)
                         next_ind.append(next_index)
-                        len = len(next_ind)
+                        length = len(next_ind)
             
-            if (len>1) & (l == 0):
+            if (length>1) & (l == 0):
                 move_robot(next_ind[m])
                 m+=1
                 l+=1
                 last_val = check_wall
-                len-=1
+                length-=1
                 pos_vals_rob(l)
-            elif (len>1) & (l>0) & (next_val>last_val):
+            elif (length>1) & (l>0) & (next_val>last_val):
                 reverse_robot(next_ind[m-1])
                 move_robot(next_ind[m])
-                len-=1
+                length-=1
                 m+=1
                 l+=1
                 last_val = check_wall
                 pos_vals_rob(l)
             else:
                 move_robot(next_ind[0])
+    
 
-            
+    """Code for shortest path"""
+    robot_move = wavemat
 
+    """Place robot at start"""
+    pos_robot = np.where(wallmat == 2)
+    robotpos_0 = pos_robot[0]
+    robotpos_1 = pos_robot[1]
+    global robotpos
+    robotpos = [robotpos_0[0], robotpos_1[0]]
+    global i
+    global j
+    i = robotpos[0]
+    j = robotpos[1]
+    print(i)
+
+    global length
+    length = 0
+    global next_val, last_val
+    next_val = 0
+    last_val = 0
+    global next_ind
+    next_ind = []
+
+    """Check if robot is next to the goal (3 represents goal)"""
+    posr = np.where(wallmat == 3)
+    posr_0 = posr[0]
+    posr_1 = posr[1]
+    goal = [posr_0[0], posr_1[0]]
+
+    """Directions robot can move"""
+    north = robot_move[(i+1), j]
+    east = robot_move[i, (j+1)]
+    south = robot_move[(i-1), j]
+    west = robot_move[i, (j-1)]
+
+
+    """Finding the smallest numeric value for robot path"""
+    lst = [north, east, south, west]
+    #lst = [5,2,7,4]
+    lst_str = [str(x) for x in lst]
+    lst.sort()
+    lst_str.sort()
+    smallest_num = np.copy(lst)
+    smallest = np.copy(lst_str)
+    #print(smallest_num)
 
 
     pos_vals_rob(0)
+    global reached_goal
+    reached_goal = 0 
+    pos_vals_rob(0)
 
+    
+
+
+"""Run Robot Path"""
+shortest_path()
 
 
 
@@ -301,3 +374,5 @@ visited = []
 
 # Call search function
 breadth_first_search() 
+
+
